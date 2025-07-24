86a131f668a6436e605ebd8cf3bed298c572a706
sanzoku05181 <51223219+sanzoku05181@users.noreply.github.com>
2025-07-24 15:48:46 +0900

    Update GmapApiTest.py

--- GmapApiTest.py
+++ GmapApiTest.py
@@ -1,2 +1,2 @@
 import googlemaps;
-print(googlemaps.Client(key='AIzaSyDsJ7le3-B4HGY3o-3d-KtpvO8DTdKxhIc').distance_matrix('札幌駅', 'JR太美駅'))
+print(googlemaps.Client(key='').distance_matrix('札幌駅', 'JR太美駅'))
